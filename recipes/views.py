3de7c57baf2ff273539f844bf2877064705f943a
Danilo Campos Ruiz <danilocamposruiz@gmail.com>
2023-05-03 17:26:41 -0300

    split home and recipe templates

--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Danilo',
-
     })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Danilo',
-
     })

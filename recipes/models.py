7a0507d3802c4a2e84b9623d3f9d43ad055d6b71
Danilo Campos Ruiz <danilocamposruiz@gmail.com>
2023-06-14 16:01:32 -0300

    update models.py

--- recipes/models.py
+++ recipes/models.py
@@ -13,12 +13,12 @@ class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
-    preparation_time = models.ImageField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=165)
-    servings = models.ImageField()
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=165)
     preparation_steps = models.TextField()
-    preparation_steps = models.BooleanField(default=False)
+    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
@@ -26,5 +26,8 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
